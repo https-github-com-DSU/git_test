168fba76f6fb224619f457b780fa77de904150fc
Han <f27978329@gmail.com>
2020-06-03 17:28:26 +0800

    [feature] update preprocess pipeline (for validation)

--- data_preprocess.py
+++ data_preprocess.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 
 from keras.utils.np_utils import to_categorical # convert to one-hot-encoding
+from sklearn.model_selection import train_test_split
 
 
 # Load the data
@@ -28,9 +29,16 @@ test = test.values.reshape(-1,28,28,1)
 # Encode labels to one hot vectors (ex : 2 -> [0,0,1,0,0,0,0,0,0,0])
 Y_train = to_categorical(Y_train, num_classes = 10)
 
+# Set the random seed
+random_seed = 2
+# Split the train and the validation set for the fitting
+X_train, X_val, Y_train, Y_val = train_test_split(X_train, Y_train, test_size = 0.1, random_state=random_seed)
+
 if not os.path.exists('dataset/preprocess'):
     os.makedirs('dataset/preprocess')
 
 np.save("dataset/preprocess/X_train_processed", X_train)
 np.save("dataset/preprocess/Y_train_processed", Y_train)
+np.save("dataset/preprocess/X_val_processed", X_val)
+np.save("dataset/preprocess/Y_val_processed", Y_val)
 
