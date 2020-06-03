423ad56eea83a2e2bdb72dc9556568b43532d6d7
Han <f27978329@gmail.com>
2020-06-03 16:46:27 +0800

    [feature] training pipeline (preprocessed) & .dvc file

---
+++ train.py
@@ -0,0 +1,73 @@
+from keras.utils.np_utils import to_categorical # convert to one-hot-encoding
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D
+from keras.optimizers import RMSprop
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import ReduceLROnPlateau
+
+from sklearn.model_selection import train_test_split
+import numpy as np
+
+
+# read clean files
+X_train = np.load("dataset/preprocess/X_train_processed.npy")
+Y_train = np.load("dataset/preprocess/Y_train_processed.npy")
+print(X_train.shape)
+
+# Set the CNN model
+model = Sequential()
+
+model.add(Conv2D(filters = 32, kernel_size = (5,5),padding = 'Same',
+                 activation ='relu', input_shape = (28,28,1)))
+model.add(Conv2D(filters = 32, kernel_size = (5,5),padding = 'Same',
+                 activation ='relu'))
+model.add(MaxPool2D(pool_size=(2,2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(filters = 64, kernel_size = (3,3),padding = 'Same',
+                 activation ='relu'))
+model.add(Conv2D(filters = 64, kernel_size = (3,3),padding = 'Same',
+                 activation ='relu'))
+model.add(MaxPool2D(pool_size=(2,2), strides=(2,2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(256, activation = "relu"))
+model.add(Dropout(0.5))
+model.add(Dense(10, activation = "softmax"))
+
+# Define the optimizer
+optimizer = RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=0.0)
+
+# Compile the model
+model.compile(optimizer = optimizer , loss = "categorical_crossentropy", metrics=["accuracy"])
+
+# Set a learning rate annealer
+learning_rate_reduction = ReduceLROnPlateau(monitor='val_accuracy',
+                                            patience=3,
+                                            verbose=1,
+                                            factor=0.5,
+                                            min_lr=0.00001)
+epochs = 1 # Turn epochs to 30 to get 0.9967 accuracy
+batch_size = 86
+
+# Set the random seed
+random_seed = 2
+# Split the train and the validation set for the fitting
+X_train, X_val, Y_train, Y_val = train_test_split(X_train, Y_train, test_size = 0.1, random_state=random_seed)
+
+# Without data augmentation i obtained an accuracy of 0.98114
+history = model.fit(X_train, Y_train, batch_size = batch_size, epochs = epochs,
+         validation_data = (X_val, Y_val), verbose = 2)
+
+model.save('./model/model.h5')
+
+
+
+
+
+
+
+
+
+
